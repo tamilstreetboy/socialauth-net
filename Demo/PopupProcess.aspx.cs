24b87c620fd212c933bcd3f9968fda4de315a4df
Deepak Aggarwal <deepak.aggarwal@3pillarglobal.com>
2012-01-04 11:51:36 +0000

    changes for v2.2
    git-svn-id: https://socialauth-net.googlecode.com/svn/trunk/Solution@182 00d33626-69db-1b77-84f5-0f0ee36482b4

--- Demo/PopupProcess.aspx.cs
+++ Demo/PopupProcess.aspx.cs
@@ -21,9 +21,13 @@ namespace Brickred.SocialAuth.NET.Demo
                     SocialAuthUser.GetCurrentUser().Login(providerType, "popupprocess.aspx", errorRedirectURL: "popupprocess.aspx");
                     ClientScript.RegisterClientScriptBlock(this.GetType(), "closeWin", "<script>window.close()</script>");
                 }
-                else if(Request.QueryString["error_message"]!=null)
+                else if (Request.QueryString["error_message"] != null)
                 {
                     ClientScript.RegisterStartupScript(this.GetType(), "closeWin", "<script>alert('" + Request.QueryString["error_message"] + "'); window.close()</script>");
+                }
+                else
+                {
+                    ClientScript.RegisterClientScriptBlock(this.GetType(), "closeWin", "<script>window.close()</script>");
                 }
             }
             catch (Exception ex)
