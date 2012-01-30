2965c5a0618d104650d409ebc250831288cd288a
Deepak Aggarwal <deepak.aggarwal@3pillarglobal.com>
2012-01-30 07:43:27 +0000

    STSInstaller
    git-svn-id: https://socialauth-net.googlecode.com/svn/trunk@291 00d33626-69db-1b77-84f5-0f0ee36482b4

---
+++ SocialAuthSTS/CustomSocialAuthSTSInstaller/SelectProviders.Designer.cs
@@ -0,0 +1,384 @@
+﻿namespace CustomSocialAuthSTSInstaller
+{
+    partial class SelectProviders
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblSelectProviders = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlProviders = new System.Windows.Forms.Panel();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.chkYahoo = new System.Windows.Forms.CheckBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.chkMySpace = new System.Windows.Forms.CheckBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.chkMSN = new System.Windows.Forms.CheckBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.chkLinkedIn = new System.Windows.Forms.CheckBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.chkGoogle = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chkFB = new System.Windows.Forms.CheckBox();
+            this.picTwitter = new System.Windows.Forms.PictureBox();
+            this.chkTwitter = new System.Windows.Forms.CheckBox();
+            this.rdCustomProviders = new System.Windows.Forms.RadioButton();
+            this.rdDefaultProvider = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.pnlProviders.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTwitter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.lblSelectProviders);
+            this.panel1.Location = new System.Drawing.Point(-5, -4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(494, 71);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblSelectProviders
+            // 
+            this.lblSelectProviders.AutoSize = true;
+            this.lblSelectProviders.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSelectProviders.ForeColor = System.Drawing.Color.Black;
+            this.lblSelectProviders.Location = new System.Drawing.Point(17, 16);
+            this.lblSelectProviders.Name = "lblSelectProviders";
+            this.lblSelectProviders.Size = new System.Drawing.Size(226, 18);
+            this.lblSelectProviders.TabIndex = 0;
+            this.lblSelectProviders.Text = "Environment Configuration";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnBack);
+            this.groupBox1.Controls.Add(this.btnNext);
+            this.groupBox1.Controls.Add(this.btnCancel);
+            this.groupBox1.Location = new System.Drawing.Point(-4, 323);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(493, 52);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(303, 15);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(83, 23);
+            this.btnBack.TabIndex = 3;
+            this.btnBack.Text = "< Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(395, 15);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(83, 23);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "Next >";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(211, 15);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(83, 23);
+            this.btnCancel.TabIndex = 0;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 287);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(408, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Note: This mode will make a required entry for \"opensource.brickred.com\" in host " +
+                "file.";
+            // 
+            // pnlProviders
+            // 
+            this.pnlProviders.Controls.Add(this.pictureBox6);
+            this.pnlProviders.Controls.Add(this.chkYahoo);
+            this.pnlProviders.Controls.Add(this.pictureBox5);
+            this.pnlProviders.Controls.Add(this.chkMySpace);
+            this.pnlProviders.Controls.Add(this.pictureBox4);
+            this.pnlProviders.Controls.Add(this.chkMSN);
+            this.pnlProviders.Controls.Add(this.pictureBox3);
+            this.pnlProviders.Controls.Add(this.chkLinkedIn);
+            this.pnlProviders.Controls.Add(this.pictureBox2);
+            this.pnlProviders.Controls.Add(this.chkGoogle);
+            this.pnlProviders.Controls.Add(this.pictureBox1);
+            this.pnlProviders.Controls.Add(this.chkFB);
+            this.pnlProviders.Controls.Add(this.picTwitter);
+            this.pnlProviders.Controls.Add(this.chkTwitter);
+            this.pnlProviders.Location = new System.Drawing.Point(2, 97);
+            this.pnlProviders.Name = "pnlProviders";
+            this.pnlProviders.Size = new System.Drawing.Size(482, 159);
+            this.pnlProviders.TabIndex = 18;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::CustomSocialAuthSTSInstaller.Properties.Resources.yahoo;
+            this.pictureBox6.Location = new System.Drawing.Point(130, 92);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(50, 53);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox6.TabIndex = 28;
+            this.pictureBox6.TabStop = false;
+            // 
+            // chkYahoo
+            // 
+            this.chkYahoo.AutoSize = true;
+            this.chkYahoo.Location = new System.Drawing.Point(109, 92);
+            this.chkYahoo.Name = "chkYahoo";
+            this.chkYahoo.Size = new System.Drawing.Size(15, 14);
+            this.chkYahoo.TabIndex = 27;
+            this.chkYahoo.Tag = "Yahoo";
+            this.chkYahoo.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::CustomSocialAuthSTSInstaller.Properties.Resources.myspace;
+            this.pictureBox5.Location = new System.Drawing.Point(31, 92);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox5.TabIndex = 26;
+            this.pictureBox5.TabStop = false;
+            // 
+            // chkMySpace
+            // 
+            this.chkMySpace.AutoSize = true;
+            this.chkMySpace.Location = new System.Drawing.Point(10, 92);
+            this.chkMySpace.Name = "chkMySpace";
+            this.chkMySpace.Size = new System.Drawing.Size(15, 14);
+            this.chkMySpace.TabIndex = 25;
+            this.chkMySpace.Tag = "Myspace";
+            this.chkMySpace.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::CustomSocialAuthSTSInstaller.Properties.Resources.msn;
+            this.pictureBox4.Location = new System.Drawing.Point(423, 14);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 24;
+            this.pictureBox4.TabStop = false;
+            // 
+            // chkMSN
+            // 
+            this.chkMSN.AutoSize = true;
+            this.chkMSN.Location = new System.Drawing.Point(402, 14);
+            this.chkMSN.Name = "chkMSN";
+            this.chkMSN.Size = new System.Drawing.Size(15, 14);
+            this.chkMSN.TabIndex = 23;
+            this.chkMSN.Tag = "MSN";
+            this.chkMSN.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::CustomSocialAuthSTSInstaller.Properties.Resources.linkedin;
+            this.pictureBox3.Location = new System.Drawing.Point(324, 14);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 22;
+            this.pictureBox3.TabStop = false;
+            // 
+            // chkLinkedIn
+            // 
+            this.chkLinkedIn.AutoSize = true;
+            this.chkLinkedIn.Location = new System.Drawing.Point(303, 14);
+            this.chkLinkedIn.Name = "chkLinkedIn";
+            this.chkLinkedIn.Size = new System.Drawing.Size(15, 14);
+            this.chkLinkedIn.TabIndex = 21;
+            this.chkLinkedIn.Tag = "LinkedIn";
+            this.chkLinkedIn.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::CustomSocialAuthSTSInstaller.Properties.Resources.google;
+            this.pictureBox2.Location = new System.Drawing.Point(228, 14);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 53);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 20;
+            this.pictureBox2.TabStop = false;
+            // 
+            // chkGoogle
+            // 
+            this.chkGoogle.AutoSize = true;
+            this.chkGoogle.Location = new System.Drawing.Point(207, 14);
+            this.chkGoogle.Name = "chkGoogle";
+            this.chkGoogle.Size = new System.Drawing.Size(15, 14);
+            this.chkGoogle.TabIndex = 19;
+            this.chkGoogle.Tag = "Google";
+            this.chkGoogle.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CustomSocialAuthSTSInstaller.Properties.Resources.facebook;
+            this.pictureBox1.Location = new System.Drawing.Point(130, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // chkFB
+            // 
+            this.chkFB.AutoSize = true;
+            this.chkFB.Location = new System.Drawing.Point(109, 14);
+            this.chkFB.Name = "chkFB";
+            this.chkFB.Size = new System.Drawing.Size(15, 14);
+            this.chkFB.TabIndex = 17;
+            this.chkFB.Tag = "Facebook";
+            this.chkFB.UseVisualStyleBackColor = true;
+            // 
+            // picTwitter
+            // 
+            this.picTwitter.Image = global::CustomSocialAuthSTSInstaller.Properties.Resources.twitter;
+            this.picTwitter.Location = new System.Drawing.Point(31, 14);
+            this.picTwitter.Name = "picTwitter";
+            this.picTwitter.Size = new System.Drawing.Size(50, 50);
+            this.picTwitter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picTwitter.TabIndex = 16;
+            this.picTwitter.TabStop = false;
+            // 
+            // chkTwitter
+            // 
+            this.chkTwitter.AutoSize = true;
+            this.chkTwitter.Location = new System.Drawing.Point(10, 14);
+            this.chkTwitter.Name = "chkTwitter";
+            this.chkTwitter.Size = new System.Drawing.Size(15, 14);
+            this.chkTwitter.TabIndex = 15;
+            this.chkTwitter.Tag = "Twitter";
+            this.chkTwitter.UseVisualStyleBackColor = true;
+            // 
+            // rdCustomProviders
+            // 
+            this.rdCustomProviders.AutoSize = true;
+            this.rdCustomProviders.Location = new System.Drawing.Point(2, 74);
+            this.rdCustomProviders.Name = "rdCustomProviders";
+            this.rdCustomProviders.Size = new System.Drawing.Size(372, 17);
+            this.rdCustomProviders.TabIndex = 20;
+            this.rdCustomProviders.TabStop = true;
+            this.rdCustomProviders.Text = "(Custom) Explicitly select providers and specific keys, secrets and scopes.";
+            this.rdCustomProviders.UseVisualStyleBackColor = true;
+            // 
+            // rdDefaultProvider
+            // 
+            this.rdDefaultProvider.AutoSize = true;
+            this.rdDefaultProvider.Location = new System.Drawing.Point(2, 265);
+            this.rdDefaultProvider.Name = "rdDefaultProvider";
+            this.rdDefaultProvider.Size = new System.Drawing.Size(399, 17);
+            this.rdDefaultProvider.TabIndex = 21;
+            this.rdDefaultProvider.TabStop = true;
+            this.rdDefaultProvider.Text = "(Demo) Automatic setup with all providers using opensource.brickred.com demo";
+            this.rdDefaultProvider.UseVisualStyleBackColor = true;
+            // 
+            // SelectProviders
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 373);
+            this.Controls.Add(this.rdDefaultProvider);
+            this.Controls.Add(this.rdCustomProviders);
+            this.Controls.Add(this.pnlProviders);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SelectProviders";
+            this.ShowInTaskbar = false;
+            this.Text = "STSInstaller";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.SelectProviders_FormClosed);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.pnlProviders.ResumeLayout(false);
+            this.pnlProviders.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTwitter)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblSelectProviders;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Panel pnlProviders;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.CheckBox chkYahoo;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.CheckBox chkMySpace;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.CheckBox chkMSN;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.CheckBox chkLinkedIn;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.CheckBox chkGoogle;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox chkFB;
+        private System.Windows.Forms.PictureBox picTwitter;
+        private System.Windows.Forms.CheckBox chkTwitter;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.RadioButton rdCustomProviders;
+        private System.Windows.Forms.RadioButton rdDefaultProvider;
+
+    }
+}
